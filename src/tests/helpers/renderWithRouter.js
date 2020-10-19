79d3841245b88cc4fc156ada46696cb02dc06583
Dilenio Enderle <dilenio@gmail.com>
2020-10-19 19:22:42 -0300

    1. Testando o arquivo App.js - eslint corrections

--- src/tests/helpers/renderWithRouter.js
+++ src/tests/helpers/renderWithRouter.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
-// para modificar eslint
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
-  return ({ ...render(<Router history={history}>{component}</Router>), history, });
+  return ({ ...render(<Router history={ history }>{ component }</Router>), history });
 };
 
 export default renderWithRouter;
