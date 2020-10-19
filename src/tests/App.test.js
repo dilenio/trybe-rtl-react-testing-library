201b65cca0274943a04a22d79e1642e4a1c1c9ef
Dilenio Enderle <dilenio@gmail.com>
2020-10-19 19:16:09 -0300

    1. Testando o arquivo App.js - eslint corrections

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,46 +1,28 @@
 import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
 import { fireEvent } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
-// test('renders a reading with the text `Pokédex`', () => {
-//   const { getByText } = render(
-//     <MemoryRouter>
-//       <App />
-//     </MemoryRouter>,
-//   );
-//   const heading = getByText(/Pokédex/i);
-//   expect(heading).toBeInTheDocument();
-// });
 describe('1. Testando o arquivo App.js', () => {
-  it('Teste se a página principal da Pokédex é renderizada ao carregar a aplicação no caminho de URL /.', () => {
+  it('Teste se a página principal da Pokédex é renderizada', () => {
     const { getByText } = renderWithRouter(<App />);
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
 
-  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const home = getByText(/Home/i);
-    const about = getByText(/About/i);
-    const favorite = getByText(/Favorite Pokémons/i);
-
     expect(home).toBeInTheDocument();
     fireEvent.click(home);
-    const pathHome = history.location.pathname;
-    expect(pathHome).toBe('/');
-
+    expect(history.location.pathname).toBe('/');
+    const about = getByText(/About/i);
     expect(about).toBeInTheDocument();
     fireEvent.click(about);
-    const pathAbout = history.location.pathname;
-    expect(pathAbout).toBe('/about');
-
+    expect(history.location.pathname).toBe('/about');
+    const favorite = getByText(/Favorite Pokémons/i);
     expect(favorite).toBeInTheDocument();
     fireEvent.click(favorite);
-    const pathFavorites = history.location.pathname;
-    expect(pathFavorites).toBe('/favorites');
+    expect(history.location.pathname).toBe('/favorites');
   });
-
-
 });
