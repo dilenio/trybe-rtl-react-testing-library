de48a442c1973089f25f1357c68513fed828287d
Dilenio Enderle <dilenio@gmail.com>
2020-10-20 06:10:36 -0300

    4. Testando o arquivo NotFound.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,15 +12,15 @@ describe('1. Testando o arquivo App.js', () => {
 
   it('Teste se o topo da aplicação contém um conjunto fixo de links', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const home = getByText('Home');
+    const home = getByText(/Home/i);
     expect(home).toBeInTheDocument();
     fireEvent.click(home);
     expect(history.location.pathname).toBe('/');
-    const about = getByText('About');
+    const about = getByText(/About/i);
     expect(about).toBeInTheDocument();
     fireEvent.click(about);
     expect(history.location.pathname).toBe('/about');
-    const favorite = getByText('Favorite Pokémons');
+    const favorite = getByText(/Favorite Pokémons/i);
     expect(favorite).toBeInTheDocument();
     fireEvent.click(favorite);
     expect(history.location.pathname).toBe('/favorites');
@@ -28,21 +28,21 @@ describe('1. Testando o arquivo App.js', () => {
 
   it('Teste se a aplicação é redirecionada para a página inicial', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const home = getByText('Home');
+    const home = getByText(/Home/i);
     fireEvent.click(home);
     expect(history.location.pathname).toBe('/');
   });
 
   it('Teste se a aplicação é redirecionada para a página de About', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const about = getByText('About');
+    const about = getByText(/About/i);
     fireEvent.click(about);
     expect(history.location.pathname).toBe('/about');
   });
 
   it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritado', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const favorite = getByText('Favorite Pokémons');
+    const favorite = getByText(/Favorite Pokémons/i);
     fireEvent.click(favorite);
     expect(history.location.pathname).toBe('/favorites');
   });
@@ -51,7 +51,7 @@ describe('1. Testando o arquivo App.js', () => {
     const { getByText, history } = renderWithRouter(<App />);
     history.push('/notfound');
     expect(history.location.pathname).toBe('/notfound');
-    const notFound = getByText(/Page requested not found/);
+    const notFound = getByText(/Page requested not found/i);
     expect(notFound).toBeInTheDocument();
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('4. Testando o arquivo NotFound.js', () => {
+  test('Teste se página contém um heading h2 - Page requested not found', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const heading = getByRole('heading', { name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+
+    const image = getAllByRole('img')[1];
+    expect(image).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'
+    )
+  });
+});
